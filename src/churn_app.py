4127babaece321f2e424af537a80079184ac83d7
AnupamMisra <innocentangelfromhell@gmail.com>
2021-06-30 16:27:56 +0530

    cleaned the app

--- src/churn_app.py
+++ src/churn_app.py
@@ -39,55 +39,6 @@ def uploader():
     else:
         st.markdown("Waiting for your input...(You can find a filled template at my [github](https://github.com/coderkol95/Data-science-projects/blob/master/Customer_churn/Data/Telco-Customer-Churn.csv) repo.)",unsafe_allow_html=True)    
 
-'''
-def preprocess(df):
-
-    df['gender'] = (df.gender=='Male').astype('int')
-    df['SeniorCitizen'] = (df.SeniorCitizen=='Yes').astype('int')
-    df['Partner'] =( df.Partner=='Yes').astype('int')
-    df['Dependents'] = (df.Dependents=='Yes').astype('int')
-    df['PhoneService'] = (df.PhoneService=='Yes').astype('int')
-    df['PaperlessBilling'] = (df.PaperlessBilling=='Yes').astype('int')
-    df['MultipleLines_No'] = (df.MultipleLines=="No").astype('int')
-    df['MultipleLines_Yes'] = (df.MultipleLines=="Yes").astype('int')
-    df['MultipleLines_No phone service'] = (df.MultipleLines=="No phone service").astype('int')
-    df['InternetService_Fiber optic'] = (df.InternetService=="Fibre optic").astype('int')
-    df['InternetService_No'] =( df.InternetService=="No").astype('int')
-    df['InternetService_DSL'] = (df.InternetService=="DSL").astype('int')
-    df['OnlineSecurity_No'] = (df.OnlineSecurity=="No").astype('int')
-    df['OnlineSecurity_No internet service'] = (df.OnlineSecurity=="No internet service").astype('int')
-    df['OnlineSecurity_Yes'] =( df.OnlineSecurity=="Yes").astype('int')
-    df['OnlineBackup_No'] = (df.OnlineBackup=='No').astype('int')
-    df['OnlineBackup_No internet service'] = (df.OnlineBackup=='No internet service').astype('int')
-    df['OnlineBackup_Yes'] = (df.OnlineBackup=='Yes').astype('int')
-    df['DeviceProtection_No'] = (df.DeviceProtection=="No").astype('int')
-    df['DeviceProtection_No internet service'] = (df.DeviceProtection=="No internet service").astype('int')
-    df['DeviceProtection_Yes'] =( df.DeviceProtection=="Yes").astype('int')
-    df['TechSupport_No internet service'] = (df.TechSupport=="No internet service").astype('int')
-    df['TechSupport_Yes']= (df.TechSupport=="Yes").astype('int')
-    df['TechSupport_No']= (df.TechSupport=="No").astype('int')
-    df['StreamingTV_No'] =( df.StreamingTV=="No").astype('int')
-    df['StreamingTV_No internet service'] = (df.StreamingTV=="No internet service").astype('int')
-    df['StreamingTV_Yes'] = (df.StreamingTV=="Yes").astype('int')
-    df['StreamingMovies_No'] = (df.StreamingMovies=="No").astype('int')
-    df['StreamingMovies_No internet service'] = (df.StreamingMovies=="No internet service").astype('int')
-    df['StreamingMovies_Yes'] = (df.StreamingMovies=="Yes").astype('int')
-    df['Contract_Month-to-month']=(df.Contract=='Month-to-month').astype('int')
-    df['Contract_One year']=(df.Contract=='One year').astype('int')
-    df['Contract_Two year']=(df.Contract=='Two year').astype('int')
-    df['PaymentMethod_Bank transfer (automatic)'] = (df.PaymentMethod=="Bank transfer (automatic)").astype('int')
-    df['PaymentMethod_Credit card (automatic)'] = (df.PaymentMethod=="Credit card (automatic)").astype('int')
-    df['PaymentMethod_Electronic check'] = (df.PaymentMethod=="Electronic check").astype('int')
-    df['PaymentMethod_Mailed check'] = (df.PaymentMethod=="Mailed check").astype('int')
-
-    with open('./Customer_churn/deployment/scalar','rb') as a:
-        scalar=pickle.load(a)
-
-    df[['tenure','MonthlyCharges','TotalCharges']] = scalar.transform(df[['tenure','MonthlyCharges','TotalCharges']])
-    df.drop(['MultipleLines', 'InternetService','OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport','StreamingTV', 'StreamingMovies', 'Contract','PaymentMethod'],axis=1,inplace=True)
-    return df
-'''
-
 if __name__=='__main__':
     
     st.title("Phone company's churn prediction module for the Martian Sapiens")
