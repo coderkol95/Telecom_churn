29278f95575391f48cd5fb35eabae676eb993d98
AnupamMisra <innocentangelfromhell@gmail.com>
2021-07-05 17:59:27 +0530

    added comments

--- src/Modelling.py
+++ src/Modelling.py
@@ -8,27 +8,48 @@ from functools import partial
 from sklearn.metrics import recall_score, precision_score,accuracy_score,f1_score
 import pickle
 
+
+"""
+
+Modelling:
+
+Different machine learning models were trained on the train data and tested on the test data.
+The top three models' hyperparameteres were tuned to obtain the best performance.
+AdaBoost performed the best.
+
+Here AdaBoost is being trained on the whole dataset for deployment.
+
+"""
+
+#Reading the dataset
 dataset = pd.read_csv(r"./data/dataset.csv")
 
-
+#Splitting the dataset into features and target
 X = dataset.iloc[:,:-1]
 y = dataset.iloc[:,-1]
-'''
+
+
+#The function to be optimized by the HyperOpt
 def optimize(X,y,params):
 
     s=StratifiedKFold(n_splits=5)
     mod = AdaBoostClassifier(**params)
     score=cross_val_score(mod,X,y,scoring='f1',cv=s).mean()    
     return -score.mean()
+
+#Hyperparameters to be tuned
 param_space_gb =  {
         'learning_rate': hp.uniform('lr',0.01,1),
         'n_estimators': scope.int(hp.quniform('trees',50,1000,1))
 }
 
+#To store the results of the trials
 trials=Trials()
 
+#The optimization function
 optimization_function= partial(optimize,X,y)
 
+#The final assessment
 result = fmin(
     fn=optimization_function,
     max_evals=20,
@@ -36,10 +57,10 @@ result = fmin(
     trials=trials,
     algo=tpe.suggest
 )
-'''
-#print(result)
+
+print(result)
 #model = AdaBoostClassifier(learning_rate= result['lr'], n_estimators= int(result['trees']))
-model = AdaBoostClassifier(learning_rate= 0.877214687401072, n_estimators=62)
+model = AdaBoostClassifier(learning_rate= 0.877214687401072, n_estimators=62)                   #These were obtained as the best results. Running it repeatedly causes computation overhead.
 
 model.fit(X,y)
 
--- src/Preprocessing.py
+++ src/Preprocessing.py
@@ -4,25 +4,24 @@ from sklearn.compose import ColumnTransformer as CT
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from sklearn.model_selection import train_test_split 
 import pickle
- 
+
+#Reading the data 
 df = pd.read_csv(r"./data/Telco-Customer-Churn.csv")
+target=df.Churn
 df.drop(["customerID"], axis=1, inplace=True)
 
 df.SeniorCitizen=df.SeniorCitizen.apply(lambda x: str(x))
 
+
+#Splitting features based on type for further preprocessing
 binary_feat = df.nunique()[df.nunique() == 2].keys().tolist()
 numeric_feat = [col for col in df.select_dtypes([np.float64,np.int64]).columns.tolist() if col not in binary_feat]
 categorical_feat = [ col for col in df.select_dtypes('object').columns.to_list() if col not in binary_feat + numeric_feat ]
 
+#Removing the target variable
 binary_feat.remove('Churn')
-target=df.Churn
 
-'''
-nos=["No phone service", "No"]
-nos.extend( ["No internet service"]*6)
-nos.extend(['Two year','Mailed check'])
-'''
-
+#Creating a column transformer
 preprocessing = CT(
                     transformers=[
                         ('numeric_scaling', MinMaxScaler(), numeric_feat),
@@ -33,29 +32,45 @@ preprocessing = CT(
                     n_jobs=-1
                     )
 
+
+
+#Creating train and test splits
 X_train, X_test, y_train, y_test = train_test_split(df.drop(['Churn'],axis=1),target, test_size=0.2, random_state=123456)
 
+#Fitting the transformer
 X_train = pd.DataFrame(preprocessing.fit_transform(X_train))
 X_test = pd.DataFrame(preprocessing.transform(X_test))
 
+#Label encoding.
 y_train = y_train.map({'Yes':1,'No':0})
 y_test = y_test.map({'Yes':1,'No':0})
 
+
+#Preparing datasets for use in modelling phase
 train = pd.concat([X_train, pd.DataFrame(y_train.values)], axis=1)
 test = pd.concat([X_test, pd.DataFrame(y_test.values)], axis=1)
 
+
+#Also preparing the entire dataset for use by production model
 dataset_X=df.drop(['Churn'],axis=1)
 dataset_y=df['Churn']
 dataset_y = dataset_y.map({'Yes':1,'No':0})
 
+#The final transformer to be used by the production model in deployment phase
 prep=preprocessing.fit(dataset_X)
+
+#Transforming the entire dataset based on the fit
 dataset_X = pd.DataFrame(prep.transform(dataset_X))
 
+#The final dataset to be used for production model
 dataset = pd.concat([dataset_X,dataset_y],axis=1)
 
+#Writing out the preprocessing transformer
 with open(r'./bin/preprocessing.pkl','wb') as r:
     pickle.dump(prep,r) 
 
+
+#Writing out the files for use in modelling
 train.to_csv(r'./data/train.csv', index=False)
 test.to_csv(r'./data/test.csv',index=False)
 dataset.to_csv(r'./data/dataset.csv',index=False)
