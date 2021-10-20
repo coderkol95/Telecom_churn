c08128474fd047333f63991bbe75c8d738d9a3d5
AnupamMisra <innocentangelfromhell@gmail.com>
2021-10-20 23:43:17 +0530

    changed datatype

--- src/churn_app.py
+++ src/churn_app.py
@@ -151,9 +151,9 @@ if __name__=='__main__':
                 
                 out=pd.DataFrame(y,index=custID)
                 out.columns=['Likely to churn']
-                out['Likely to churn'].replace({'1':'Yes','0':'No'}, inplace=True)
+                out['Likely to churn'].astype('str')
+                out['Likely to churn'] = out['Likely to churn'].replace({'1':'Yes','0':'No'}, inplace=True)
                 out.index.name='Customer ID'
-                out['Likely to churn'].astype('bool')
                 st.write(out)
                 out.reset_index(inplace=True)
                 
